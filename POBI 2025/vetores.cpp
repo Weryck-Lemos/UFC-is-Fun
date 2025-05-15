21aa0d8eba87dc6d870bb8b6fb3af6d99f8527ce
Weryck <weryyck@gmail.com>
2025-05-15 10:56:58 -0300

    summit

--- POBI 2025/vetores.cpp
+++ POBI 2025/vetores.cpp
@@ -1,18 +1,56 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <algorithm> //sort, reverse, find, lb, ub, count e remove
 using namespace std;
 
 int main(){
-    int n;
-    cin>>n;
+    vector<int> a = {5, 3, 9, 2, 2, 11, 3, 1, 3, 5};
 
-    vector<int> vet(n);
-    for(int i=0; i<n; i++){
-        cin>>vet[i];
+/*
+    cout<<a.front()<<" "<<a.back()<<"\n";
+    a.pop_back();
+    a.push_back(6);
+    a.insert(a.end()-1, 60);
+*/
+/*
+    a.clear();
+    if(a.empty())cout<<"vazio\n";
+*/
+/*
+    sort(a.begin(), a.end());
+    sort(a.rbegin(), a.rend());
+
+    reverse(a.begin(), a.end());
+*/
+/*
+    int x=3;
+    auto lb = lower_bound(a.begin(), a.end(), x);
+    auto ub = upper_bound(a.begin(), a.end(), x);
+    
+    if(lb != a.end()){
+        cout<<"esta no vetor\n";
+        cout<<"lb: "<<*lb<<" "<<"ub: "<<*ub<<"\n";
+        cout<<x<<" se repete: "<<ub-lb<<" vezes\n";
     }
 
-    sort(vet.begin(), vet.end());
-    cout<<vet[ vet.size()-1]<<"\n"<<vet[0]<<"\n";
+    else{
+        cout<<"nao esta no vetor\n";
+    }
+*/
+/*
+    auto it = find(a.begin(), a.end(), 100);
+    if(it != a.end())cout<<"achei\n";
+    else cout<<"nao achei\n";
+*/
+/*
+    int tot = count(a.begin(), a.end(), 5);
+    cout<<"aparece: "<<tot<<" vezes\n";
+*/
     
+
+
+    for(int i=0; i<a.size(); i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<"\n";
 }
