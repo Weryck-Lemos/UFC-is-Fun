d43d4e33a7a5154a47495953d1dec4140b15df71
Weryck-Lemos <weryyck@gmail.com>
2026-05-14 09:47:05 -0300

    summit

--- POBI 2026/aula6/questoes/195.cpp
+++ POBI 2026/aula6/questoes/195.cpp
@@ -1,35 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> intToBinary(int n){
-    if(n==0) return {0};
-
-    vector<int> binario;
-
-    while(n>0){
-        if(n%2==0){
-            binario.push_back(0);
-        }
-
-        else{
-            binario.push_back(1);
-        }
-        n/=2;
-    }
-
-    return binario;
-
-}
-
-int main(){
+int main() {
     int n;
-    cin>>n;
+    cin >> n;
 
-    vector<int> binario = intToBinary(n);
-
-    for(int i= binario.size()-1; i>=0; i--){
-        cout<<binario[i];
+    if (n == 0) {
+        cout << 0 <<"\n";
+        return 0;
     }
 
-    cout<<endl;
+    stack<int> s;
+    while (n > 0) {
+        s.push(n % 2);
+        n /= 2;
+    }
+
+    while (!s.empty()) {
+        cout << s.top();
+        s.pop();
+    }
+    cout <<"\n";
 }
