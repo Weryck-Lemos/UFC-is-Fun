35f0a7208c7f409cdbb37d2189cb7d319a380842
Weryck-Lemos <weryyck@gmail.com>
2026-05-21 10:23:12 -0300

    summit

--- POBI 2026/aula7/questoes/2233.cpp
+++ POBI 2026/aula7/questoes/2233.cpp
@@ -1,33 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-	int n;
+int main() {
+    int n;
+    cin >>n;
 
-	cin>>n;
+    map<string, string> dic;
+    string eng, ptbr;
 
-	string eng, ptbr;
-	map<string, string> dic;
+    for (int i = 0; i < n; i++) {
+        cin >> eng >> ptbr;
+        dic[eng] = ptbr;
+    }
 
-	for (int i=0; i<n; i++){
-		cin>>eng>>ptbr;
-
-		dic[eng] = ptbr;
-	}
-
-	cin.ignore();
-
-
-	string sentence;
-	getline(cin, sentence);
-    
-	istringstream iss(sentence);
-	vector<string> words((istream_iterator<string>(iss)), istream_iterator<string>());
-
-	for (string word : words){
-		cout<<dic[word]<<" ";
-	}
-
-	cout<<endl;
-	return 0;
+    string word;
+    while (cin >> word) {
+        cout << dic[word] << " ";
+    }
+    cout << "\n";
 }
+
+
